e4d0a2d56007057e7b0c4cbd56cfb7781be45ad9
Sem Schilder <me@sem.design>
2016-12-13 17:41:50 +0100

    input name conflict

--- templates/testimonial-form.php
+++ templates/testimonial-form.php
@@ -14,7 +14,7 @@
 				<input type="hidden" id="input-rating" name="rating"><br>
 			</div>
 			<label><?php _e('Your testimonial', TROPICAL_TESTIMONIALS_TEXT_DOMAIN) ?></label><br>
-			<textarea name="testimonial" placeholder="<?php _e('Please enter your testimonial', TROPICAL_TESTIMONIALS_TEXT_DOMAIN) ?>"></textarea><br>
+			<textarea name="testimonial_text" placeholder="<?php _e('Please enter your testimonial', TROPICAL_TESTIMONIALS_TEXT_DOMAIN) ?>"></textarea><br>
 			<button class="wpcf7-form-control wpcf7-submit" name="testimonial" type="submit"><?php _e('Add testimonial', TROPICAL_TESTIMONIALS_TEXT_DOMAIN) ?></button>
 		</form>
 	</div>
