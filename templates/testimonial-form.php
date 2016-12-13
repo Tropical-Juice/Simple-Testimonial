4aef338d3018ee3fa4c1c9c579936ffde4151b25
Sem Schilder <me@sem.design>
2016-12-13 09:58:55 +0100

    Changed name for theme overrides, and changed form markup  is too general. To do: load theme overides from custom folder to avoid any conflicts

---
+++ templates/testimonial-form.php
@@ -0,0 +1,16 @@
+	<div class="testimonial" dir="ltr" lang="nl-NL" role="form" style="max-width:400px;">
+		<h1>Vul hier uw testimonial in</h1>
+		<form class="testimonial-form" enctype="multipart/form-data" id="testimonial_form" method="post" name="testimonial_form" novalidate="novalidate">
+			<label><?php _e('Company', TROPICAL_TESTIMONIALS_TEXT_DOMAIN) ?></label><br>
+			<input type="text" name="testimonial['organization']"><br>
+			<label><?php _e('Your name', TROPICAL_TESTIMONIALS_TEXT_DOMAIN) ?></label><br>
+			<input type="text" name="testimonial['name']"><br>
+			<label><?php _e('Your position', TROPICAL_TESTIMONIALS_TEXT_DOMAIN) ?></label><br>
+			<input type="text" name="testimonial['function']"><br>
+			<label><?php _e('Your rating', TROPICAL_TESTIMONIALS_TEXT_DOMAIN) ?></label><br>
+			<input type="range" name="testimonial['review']" min="0" max="10"><br>
+			<label><?php _e('Your testimonial', TROPICAL_TESTIMONIALS_TEXT_DOMAIN) ?></label><br>
+			<textarea name="testimonial[text]" placeholder="<?php _e('Please enter your testimonial', TROPICAL_TESTIMONIALS_TEXT_DOMAIN) ?>"></textarea><br>
+			<button class="wpcf7-form-control wpcf7-submit" name="pdf" type="submit"><?php _e('Add testimonial', TROPICAL_TESTIMONIALS_TEXT_DOMAIN) ?></button>
+		</form>
+	</div>
